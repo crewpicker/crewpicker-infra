0f22156593dc4801621527f153d085a04d767264
Thomas Geirhovd <tg90nor@gmail.com>
2023-05-22 07:54:18 +0200

    WIP deploy app with gha

---
+++ applications/crewpicker-rmr.tf
@@ -0,0 +1,13 @@
+module "rmr_crewpicker" {
+  source = "./modules/k8s-app"
+
+  name = "crewpicker"
+  release = "rmr"
+  replicas = 2
+  image = {
+    repo = "value"
+    tag = "value"
+  }
+  containerPort = 3000
+  hostname = "rmr.crewpicker.io"
+}
